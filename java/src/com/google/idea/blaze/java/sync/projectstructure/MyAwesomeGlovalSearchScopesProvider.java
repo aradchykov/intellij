e488dd039ff36eb9d4e45d2dbe7c83595fdc9623
Committed Chinchilla <committed.chinchilla@gmail.com>
2019-10-06 10:30:28 +0300

    small change

--- java/src/com/google/idea/blaze/java/sync/projectstructure/MyAwesomeGlovalSearchScopesProvider.java
+++ java/src/com/google/idea/blaze/java/sync/projectstructure/MyAwesomeGlovalSearchScopesProvider.java
@@ -1,5 +1,7 @@
 package com.google.idea.blaze.java.sync.projectstructure;
 
+import com.google.idea.blaze.base.model.BlazeProjectData;
+import com.google.idea.blaze.base.sync.data.BlazeProjectDataManager;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.GlobalSearchScope;
@@ -10,6 +12,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Optional;
 
 public class MyAwesomeGlovalSearchScopesProvider extends PredefinedSearchScopeProviderImpl {
 	@Override
@@ -18,9 +21,21 @@ public class MyAwesomeGlovalSearchScopesProvider extends PredefinedSearchScopePr
 	                                             boolean currentSelection, boolean usageView, boolean showEmptyScopes) {
 		List<SearchScope> predefinedScopes = super.getPredefinedScopes(project, dataContext, suggestSearchInLibs, prevSearchFiles, currentSelection, usageView, showEmptyScopes);
 
-		int index = predefinedScopes.indexOf(GlobalSearchScope.projectScope(project)); // TODO what if index is -1?
-		predefinedScopes.add(index + 1, GlobalSearchScopesCore.projectProductionScope(project));
-		predefinedScopes.add(index + 2, GlobalSearchScopesCore.projectTestScope(project));
+		BlazeProjectData projectData =
+				BlazeProjectDataManager.getInstance(project).getBlazeProjectData();
+
+		boolean hasTestTargets = Optional.ofNullable(projectData)
+				.map(data -> data
+						.getTargetMap()
+						.targets()
+						.stream()
+						.anyMatch(targetIdeInfo -> targetIdeInfo.getKind().getKindString().endsWith("_test"))
+				).orElse(false);
+		if (hasTestTargets) {
+			int index = predefinedScopes.indexOf(GlobalSearchScope.projectScope(project)); // TODO what if index is -1?
+			predefinedScopes.add(index + 1, GlobalSearchScopesCore.projectProductionScope(project));
+			predefinedScopes.add(index + 2, GlobalSearchScopesCore.projectTestScope(project));
+		}
 
 		return predefinedScopes;
 	}
